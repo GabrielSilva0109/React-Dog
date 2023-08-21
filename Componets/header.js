d1ea38c4a13da692b9978935aa2d7bd0d2648b68
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-08-21 11:24:11 -0300

    1

--- Componets/header.js
+++ Componets/header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import styles from './header.modules.css'
 const Header = () => {
     return (
         <div>
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Header from '../Componets/Header.js'
+import Footer from '../Componets/Footer.js'
 
 const App = () => {
-  return <div className="App"></div>; 
+  return(
+    <div>
+      <Header />
+      <Footer /> 
+  </div> 
+  )
+   
 }
 
 export default App
